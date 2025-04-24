3aa90122488772eb5968bdaf43019e3a666e5963
ulf <ulf.ivraeus@gmail.com>
2025-04-24 16:29:01 +0000

    Cleanup extensions, add odbc-driver etc.

---
+++ .devcontainer/installOdbcDriver.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# Started with "official guide"
+# 👉 https://learn.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver16&tabs=debian18-install%2Calpine17-install%2Cdebian8-install%2Credhat7-13-install%2Crhel7-offline#tabpanel_1_debian18-install
+#
+# The stripped away most parts as:
+# - MS repo already configured in dev-container base
+# - Not interested in "optional" parts
+#
+# Confirm successful install:
+#
+# ```console
+# $ odbcinst -q -d -n
+# [ODBC Driver 18 for SQL Server]
+# ``` 
+
+set -euxo pipefail
+
+if ! [[ "11 12" == *"$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1)"* ]];
+then
+    echo "Debian $(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1) is not currently supported.";
+    exit 1;
+fi
+
+# # Download the package to configure the Microsoft repo
+# curl -sSL -O https://packages.microsoft.com/config/debian/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2 | cut -d '.' -f 1)/packages-microsoft-prod.deb
+# # Install the package
+# sudo dpkg -i packages-microsoft-prod.deb
+# # Delete the file
+# rm packages-microsoft-prod.deb
+
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# # optional: for bcp and sqlcmd
+# sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+# echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+# source ~/.bashrc
+# # optional: for unixODBC development headers
+# sudo apt-get install -y unixodbc-dev
+# # optional: kerberos library for debian-slim distributions
+# sudo apt-get install -y libgssapi-krb5-2
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -9,4 +9,17 @@ cat ${SCRIPT_DIR}/bashrc_append >> ~/.bashrc
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y \
   bat \
-  sqlite3
+  sqlite3 \
+  postgresql-common
+
+# Matching PG CLI for local DB installation
+sudo /usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y \
+  && sudo apt update \
+  && sudo apt install -y postgresql-client-17
+
+# Enable working with pyodbc and SQL server
+# https://learn.microsoft.com/en-us/sql/connect/python/pyodbc/python-sql-driver-pyodbc
+$SCRIPT_DIR/installOdbcDriver.sh
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+
+
